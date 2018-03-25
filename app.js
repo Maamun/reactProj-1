846f7a7f99cc7e6216ef51fa8fcc5247c64db942
Mamun Mokni <mamun.mokni@gmail.com>
2018-03-25 20:03:22 +0200

    Styling 3 boxes

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const root = document.querySelector("#root");
-function tick () {
-  const time = new Date().toLocaleTimeString()
-const element = <div>IT is 
-  <input value={time} />
-  <input value={time} />
-</div>
-ReactDOM.render(element, root);
+
+function Box({ style, className = "", ...rest }) {
+  return (
+    <div
+      className={`box ${className}`.trim()}
+      style={{ padding: 20, ...style }}
+      {...rest}
+    />
+  );
 }
 
+const element = (
+  <div>
+    <Box className="box--small" style={{ backgroundColor: "lime" }}>box 1</Box>
+    <Box className="box--medium" style={{ backgroundColor: "pink" }}>box 2</Box>
+    <Box className="box--large" style={{ backgroundColor: "lightblue" }}>
+      box 3
+    </Box>
 
-// DOM version 
-// function tick () {
-//   const time = new Date().toLocaleTimeString()
-// const element = (
-//   `<div>IT is 
-//   <input value=${time} />
-//   <input value=${time} />
-// </div>`
-// )
-// root.innerHTML = element 
-// }
+  </div>
+);
 
-
- setInterval(tick, 1000);
+ReactDOM.render(element, root);
