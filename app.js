143921985587b40233a4037ccc9b44d011838a0c
Mamun Mokni <mamun.mokni@gmail.com>
2018-03-25 18:31:54 +0200

    Validate Custom React Component Props with PropTypes

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
 const root = document.querySelector("#root");
 const Message = props => <div>{props.msg}</div>;
 
-function SayHello(props) {
-  return (
-    <div>
-      Hello {props.firstName} {props.lastName}
-    </div>
-  );
-}
-const PropTypes = {
-  string(props, propName, componentName) {
-    if (typeof props[propName] !== 'string') {
-      return new Error(`Hey, you should pass a string for ${propName} in ${componentName}  but you passed a ${typeof props[propName]} !`)
-    }
+class SayHello extends React.Component {
+  static propTypes = {
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired
   }
+   render () {
+     const {firstName, lastName} = this.props
+    return (
+      <div>
+        Hello {firstName} {lastName}
+      </div>
+    )
+   }
 }
-SayHello.propTypes = {
-  firstName: PropTypes.string,
-  lastName: PropTypes.string
-}
+
 // SayHello.propTypes = {
-//   firstName(props, propName, componentName) {
-//     if (typeof props[propName] !== 'string') {
-//       return new Error(`Hey, you should pass a string for ${propName} in ${componentName}  but you passed a ${typeof props[propName]} !`)
-//     }
-//   }
+//   firstName: PropTypes.string.isRequired,
+//   lastName: PropTypes.string.isRequired
 // }
+
 ReactDOM.render(<SayHello firstName={true} />, root);
